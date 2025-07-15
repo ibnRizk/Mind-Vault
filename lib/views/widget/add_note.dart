be86afc9057409e6fd6321e710814af9423f4b62
Hussein Rizk <husseinrizk117@gmail.com>
2025-07-15 08:55:04 +0300

    create custom text field

--- lib/views/widget/add_note.dart
+++ lib/views/widget/add_note.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
 
+import '../../constant.dart';
+
 class AddNote extends StatelessWidget {
   const AddNote({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(children: [SizedBox(height: 32), CustomTextField()]),
+    );
+  }
+}
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      cursorColor: kPrimaryColor,
+      decoration: InputDecoration(
+        hintText: 'Title',
+        hintStyle: TextStyle(color: kPrimaryColor),
+        border: buildBorder(),
+        enabledBorder: buildBorder(),
+        focusedBorder: buildBorder(kPrimaryColor),
+      ),
+    );
+  }
+
+  OutlineInputBorder buildBorder([Color]) {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(16),
+      borderSide: BorderSide(color: Color ?? Colors.white),
+    );
   }
 }
